7a540cccf201c663c4601cb1a955d3859c3c51e3
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-03-11 17:10:55 -0300

    crud

---
+++ formcad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastrar</title>
+</head>
+<body>
+   
+<h1> Cadastrar Produto </h1>
+
+<form action="cadastrar.php" method="post">
+
+Produto:
+<input type="text" name="produto" />
+Quantidade:
+<input type="text" name="quantidade" />
+<input type="submit" value="OK"/>
+
+</form>
+
+<hr>
+<a href='index.php'> Voltar </a>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,10 +2,39 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Produtos</title>
 </head>
 <body>
+
+<h1> Lista de Produtos </h1>
+<hr>
+<h3> <a href="formcad.php"> Cadastrar Produto </a> </h3>
+<hr>
+<?php
+// Conectar ao BD
+include("conecta.php");
+
+// Seleciona todos os dados da tabela lista
+$sql = "SELECT * FROM produto";
+
+// Executa o Select
+$resultado = mysqli_query($conexao,$sql);
+
+//Lista as tarefas
+echo '<table border=1>
+<tr>
+<th>Produto</th>
+<th>Nomes</th>
+<th>Quantidade</th>
+</tr>';
+
+while ($dados = mysqli_fetch_assoc($resultado)) {
+echo '<tr>';    
+echo '<td>'.$dados['produto'] .'</td>';
+echo '<td>'.$dados['quantidade'] .'</td>';
+echo '<td>'.$dados['nome'] .'</td>';
+}
+?>
     
 </body>
 </html>
