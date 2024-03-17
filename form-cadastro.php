4224f3ed680ba5d9e89bbd42b77caee7db31f429
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-03-16 23:12:00 -0300

    criei o form de cadastro

---
+++ form-cadastro.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Produto</title>
+</head>
+<body>
+<form action="cadastro.php" method="post">
+        Nome: <input type="text" name="nome"><br>
+        Quantidade: <input type="text" name="quant"><br>
+        <input type="submit" value="Cadastrar"><br>
+</body>
+</html>
