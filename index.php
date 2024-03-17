982d0364a23eb1defa23d9ce225e68a7d9f86f91
Maria Clara Gomes <maria.2022315574@aluno.iffar.edu.br>
2024-03-16 23:10:43 -0300

    Criei o index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 <h1> Lista de Produtos </h1>
 <hr>
-<h3> <a href="formcad.php"> Cadastrar Produto </a> </h3>
+<h3> <a href="form-cadastro.php"> Cadastrar Produto </a> </h3>
 <hr>
 <?php
 // Conectar ao BD
@@ -23,15 +23,18 @@ $resultado = mysqli_query($conexao,$sql);
 //Lista as tarefas
 echo '<table border=1>
 <tr>
-<th>Produto</th>
+<th>ID</th>
 <th>Nomes</th>
 <th>Quantidade</th>
 </tr>';
 
 while ($dados = mysqli_fetch_assoc($resultado)) {
 echo '<tr>';   
-echo '<td>'.$dados['quantidade'] .'</td>';
+echo '<td>'.$dados['id_produto'].'</td>';
 echo '<td>'.$dados['nome'] .'</td>';
+echo '<td>'.$dados['quantidade'] .'</td>';
+echo '<td> <a href="excluir.php?id='.$dados['id_produto'].'"> Excluir</a> </td>';
+echo '<td> <a href="form-editar.php?id='.$dados['id_produto'].'"> Editar</a> </td>';
 }
 ?>
     
